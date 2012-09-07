e0323ad0020680405d292c5f858f76f3e1634a7f
Jesusaurus <jesusaurus@inbox.com>
2012-09-07 12:09:18 -0700

    yay reusing code!

---
+++ 7.py
@@ -0,0 +1,34 @@
+import math
+
+magick = 600851475143
+
+#initial set of primes
+primes = [ 2, 3, 5, 7, 11 ]
+
+def findPrimes(m):
+    while primes[-1] < m:
+        last = primes[-1]
+        for x in range(last+1, last*2):
+            f = factor(x)
+            if len(f) == 1:
+                primes.extend(f)
+        print "number of primes found: ",
+        print len(primes)
+        print "last prime: ",
+        print primes[-1]
+        print
+
+def factor(n):
+    result = []
+
+    for p in primes:
+        if n > p and n % p == 0:
+            result.append(p)
+            result.extend(factor(n/p))
+            return result
+
+    return [n]
+        
+
+findPrimes(99999)
+print primes[10000]
