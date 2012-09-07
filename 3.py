125093b469eb264fe9b4ff80cbaa9c6b7c8c244b
Jesusaurus <jesusaurus@inbox.com>
2012-09-06 23:32:07 -0700

    it works... slowly...

--- 3.py
+++ 3.py
@@ -3,22 +3,30 @@ import math
 magick = 600851475143
 
 #initial set of primes
-primes = [ 2, 3, 5, 7 ]
+primes = [ 2, 3, 5, 7, 11 ]
+
+def findPrimes(m):
+    while primes[-1] < m:
+        last = primes[-1]
+        for x in range(last+1, last*2):
+            f = factor(x)
+            if len(f) == 1:
+                primes.extend(f)
+        print "last 5 primes:"
+        print primes[-5:]
+        print
 
 def factor(n):
-    factors = []
-    primes = []
+    result = []
 
-    for x in range(2,n):
-        if n > x and n % x == 0:
-            factors.append(x)
+    findPrimes(n**0.5)
 
-    if factors:
-        for i in factors:
-            primes.extend(factor(i))
-    else:
-        primes.append(n)
+    for p in primes:
+        if n > p and n % p == 0:
+            result.append(p)
+            result.extend(factor(n/p))
+            return result
 
-    return primes
-
+    return [n]
+        
 print factor(magick)
