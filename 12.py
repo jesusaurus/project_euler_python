be1660a27f5942a29a344b566b8bc16ab3978d18
Jesusaurus <jesusaurus@inbox.com>
2012-09-20 12:10:06 -0700

    no need to recursively build a list if we just want the number of factors

--- 12.py
+++ 12.py
@@ -4,34 +4,30 @@ def triangle(n):
     return sum(range(1,n))
 
 def factor(m):
-    result = []
+    result = 0
     for i in range(2,m-1):
         if m % i == 0:
-            result.append(i)
-            result.extend(factor(i/m))
-            return result
-    return [m]
+            result += 1
+    return result
 
-def flen(s):
-    f = factor(s)
-    l = len(f)
-    del f
-    return l
-
-x = 10
+x = 2
 number = triangle(x)
 
-while flen(number) < 500:
-    x **= 2
-    number = triangle(x)
+while factor(number) < 500:
+    try:
+        x **= 2
+        number = triangle(x)
+    except MemoryError:
+        print "Not enough memory for %i\n" % x
+        break
 print "went too far"
 
-while flen(number) >= 500:
+while factor(number) >= 500:
     x /= 2
     number = triangle(x)
 print "cut back too far"
 
-while flen(number) < 500:
+while factor(number) < 500:
     x += 1
     number = triangle(x)
 print "found it"
