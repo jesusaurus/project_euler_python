207e52a258789affa13f62b4b38db9f29506c43e
Jesusaurus <jesusaurus@inbox.com>
2013-08-16 14:35:46 -0700

    Use a generator in 1

--- 1.py
+++ 1.py
@@ -1,9 +1,15 @@
 #!/bin/python
 
-total = 0
+def _gen(n):
+    x = 0
+    while x < n:
+        if (x % 3 == 0) or (x % 5 == 0):
+            yield x
+        x += 1
 
-for x in range(1, 1000):
-    if (x % 3 == 0) or (x % 5 == 0):
+
+total = 0
+for x in _gen(1000):
         total += x
 
 print total
